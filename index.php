4542274ae64fd1a1863812dec6c0785edb976d77
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-06-16 02:05:40 +0430

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,11 @@
  </head>
  <body>
  <?php echo '<p>my Hello World</p>'; ?> 
+  <?php
+echo gethostname(); // may output e.g,: sandie
+
+// Or, an option that also works before PHP 5.3
+echo php_uname('n'); // may output e.g,: sandie
+?>
  </body>
 </html>
