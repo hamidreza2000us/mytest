0fe28112f462d3037871080f8845211a844f25a1
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-06-17 02:18:44 +0430

    Update index.php

--- index.php
+++ index.php
@@ -3,12 +3,10 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>my Hello World</p>'; ?> 
-  <?php
-echo gethostname(); // may output e.g,: sandie
-
-// Or, an option that also works before PHP 5.3
-echo php_uname('n'); // may output e.g,: sandie
-?>
- </body>
+ <?php 
+  $str1= gethostname();
+  $str2= php_uname('n');
+  echo '<p>my Hello World,' . $str1 . ' ' . $str2 . '</p>';
+  ?> 
+  </body>
 </html>
